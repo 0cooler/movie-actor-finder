5042eb41b203d13025756725090fb5c6a52466b0
0cooler <justin@thehalls.us>
2025-12-30 22:22:50 -0500

    Update search.py

--- api/search.py
+++ api/search.py
@@ -2,7 +2,7 @@ import requests
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
-API_KEY = '6e31bf1b8185bda03344530cafe38caa'
+API_KEY = '4c4ae26aebfea1e40d21bc90a496e46a'
 BASE_URL = 'https://api.themoviedb.org/3'
 
 class handler(BaseHTTPRequestHandler):
